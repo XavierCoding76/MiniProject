7442460254767c817e389c5b50febe3bf5b6146a
XavierCoding76 <xavierlow0614@gmail.com>
2025-12-21 18:38:03 +0800

    Setup Firebase

--- app/src/main/java/com/example/miniproject/admin/facilityAdmin/AddFacilityScreen.kt
+++ app/src/main/java/com/example/miniproject/admin/facilityAdmin/AddFacilityScreen.kt
@@ -20,8 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.input.KeyboardType import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
